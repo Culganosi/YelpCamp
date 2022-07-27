8a0606e2f959503e07f935bedd55b158732461a1
Culganosi <culgan_@hotmail.com>
2022-07-27 14:41:47 -0400

    delete reviews

--- app.js
+++ app.js
@@ -93,6 +93,13 @@ app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async(req,res) =
 
 }))
 
+app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res) => {
+  const {id, reviewId } = req.params;
+  await Campground.findByIdAndUpdate(id, {$pull: {reviews: reviewId} })
+  await Review.findByIdAndDelete(reviewId);
+  res.redirect(`/campgrounds/${id}`);
+}))
+
 app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
   const { id } = req.params;
   await Campground.findByIdAndDelete(id);
