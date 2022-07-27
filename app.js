fc14ee1e893afce70e0301147293bc74f9dd26f1
Culganosi <culgan_@hotmail.com>
2022-07-27 13:41:12 -0400

    create reviews(db)

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
+const Review = require('./models/review')
 
 mongoose.connect('mongodb://localhost:27017/YelpCamp');
 
@@ -72,6 +73,16 @@ app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res) => {
   const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
   res.redirect(`/campgrounds/${campground._id}`);
 }));
+
+app.post('/campgrounds/:id/reviews', catchAsync(async(req,res) => {
+  const campground = await Campground.findById(req.params.id);
+  const review = new Review(req.body.review);
+  campground.reviews.push(review);
+  await review.save();
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`);
+
+}))
 
 app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
   const { id } = req.params;
