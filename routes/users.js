b97397344efab3910404dce268318baef9255a61
Culganosi <culgan_@hotmail.com>
2022-07-28 14:12:07 -0400

    add logout && navbar link

--- routes/users.js
+++ routes/users.js
@@ -28,7 +28,14 @@ router.get('/login', (req, res) => {
 
 router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
   req.flash('success', 'Welcome back!');
-  res.redirect('/campgrounds')
+  res.redirect('/campgrounds');
 });
 
+router.get('/logout', (req, res, next) => {
+  req.logout(function (err) {
+    if (err) { return next(err); }
+    req.flash('success', "Logged out");
+    res.redirect('/campgrounds');
+  });
+});
 module.exports = router;
