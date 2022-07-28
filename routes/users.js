8fb6fbbcd8060b224a8a7cd2961c811e2463767e
Culganosi <culgan_@hotmail.com>
2022-07-28 17:08:05 -0400

    restructure routes

--- routes/users.js
+++ routes/users.js
@@ -3,25 +3,23 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const catchAsync = require('../utils/catchAsync');
-const User = require('../models/user');
-const users = require('../controllers/users')
+const users = require('../controllers/users');
 
-router.get('/register', users.renderRegister)
+router.route('/register')
+  .get(users.renderRegister)
+  .post(catchAsync(users.register));
 
-router.post('/register', catchAsync(users.register))
-
-router.get('/login', users.renderLogin)
-
-router.post(
-  "/login",
-  passport.authenticate("local", {
-    failureFlash: true,
-    failureRedirect: "/login",
-    failureMessage: true,
-    keepSessionInfo: true,
-  }), users.login)
+router.route('/login')
+  .get(users.renderLogin)
+  .post(
+    passport.authenticate("local", {
+      failureFlash: true,
+      failureRedirect: "/login",
+      failureMessage: true,
+      keepSessionInfo: true,
+    }), users.login);
 
 
-router.get('/logout', users.logout)
+router.get('/logout', users.logout);
 
 module.exports = router;
