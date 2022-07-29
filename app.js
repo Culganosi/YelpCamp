5e9b25635e0fca4c7c72bf12d0dadd3357d03b67
Culganosi <culgan_@hotmail.com>
2022-07-29 17:14:05 -0400

    hiding error stack msgs

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-if(process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV !== "production") {
   require('dotenv').config();
 }
-console.log(process.env.SECRET)
+console.log(process.env.SECRET);
 
 const express = require('express');
 const path = require('path');
@@ -13,7 +13,9 @@ const session = require('express-session');
 const flash = require('connect-flash');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
-const User = require('./models/user')
+const User = require('./models/user');
+const mongoSanitize = require('express-mongo-sanitize');
+// const helmet = require('helmet');
 
 const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
@@ -36,22 +38,27 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(mongoSanitize())
+
 const sessionConfig = {
+  name: 'session',
   secret: 'notasecret',
   resave: false,
   saveUninitialized: true,
   cookie: {
     httpOnly: true,
+    // secure: true,
     expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
     maxAge: 1000 * 60 * 60 * 24 * 7
   }
-}
-app.use(session(sessionConfig))
+};
+app.use(session(sessionConfig));
 app.use(flash());
+// app.use(helmet());
 
 app.use(passport.initialize());
 app.use(passport.session());
-passport.use(new LocalStrategy(User.authenticate()))
+passport.use(new LocalStrategy(User.authenticate()));
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
@@ -61,7 +68,7 @@ app.use((req, res, next) => {
   res.locals.success = req.flash('success');
   res.locals.error = req.flash('error');
   next();
-})
+});
 
 app.use('/', userRoutes);
 app.use('/campgrounds', campgroundRoutes);
